7f50f279658ba13d2bfe7a3156dfd387b67cac1b
kzk_maeda <casino.music0711@gmail.com>
2019-10-20 23:59:11 +0900

    base created

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"templetes/layout.html",
+		"templetes/navbar.html",
+		"templetes/index.html",
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	// TODO: threads
+	fmt.Println(templates)
+}
