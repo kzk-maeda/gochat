1b5282fdc5f0eec225328341e786bb11208a85f8
kzk_maeda <casino.music0711@gmail.com>
2019-10-31 01:41:10 +0900

    mod

--- src/utils.go
+++ src/utils.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"main/data"
 	"net/http"
+	"os"
 )
 
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
 var logger *log.Logger
+
+func init() {
+	loadConfig()
+	file, err := os.OpenFile("gochat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("Cannot open config file", err)
+	}
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("Cannot get configuration from file", err)
+	}
+}
 
 func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
 	cookie, err := request.Cookie("_cookie")
